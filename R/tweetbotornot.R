6ede962698d434242316ecbe4e507ff0ea246d4d
Evan Williams <47997766+EvanUp@users.noreply.github.com>
2020-02-11 14:41:35 -0800

    Update tweetbotornot.R

--- R/tweetbotornot.R
+++ R/tweetbotornot.R
@@ -87,12 +87,13 @@ botornot.factor <- function(x, fast = FALSE) {
 
 #' @export
 botornot.character <- function(x, fast = FALSE) {
-  ## remove NA and duplicates
   x <- x[!is.na(x) & !duplicated(x)]
-  ## get most recent 100 tweets
-  x <- rtweet::get_timelines(x, n = 100)
-  ## pass to next method
-  botornot(x, fast = fast)
+    if (fast){
+      x <- rtweet::lookup_users(x)
+    }else{
+      x <- rtweet::get_timelines(x, n = 100)
+  }
+botornot(x, fast = fast)
 }
 
 
